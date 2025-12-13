c4922905403b6b7655fc65dcf485e9cac51d7070
Joe Block <jpb@unixorn.net>
2025-12-12 20:26:31 -0700

    Start adding some tooling
    Signed-off-by: Joe Block <jpb@unixorn.net>

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # kubernetes-tools
+
+[![License](https://img.shields.io/github/license/unixorn/kubernetes-tools.svg)](https://opensource.org/license/apache-2-0)
+![Awesomebot](https://github.com/unixorn/kubernetes-tools/actions/workflows/awesomebot.yml/badge.svg)
+[![GitHub stars](https://img.shields.io/github/stars/unixorn/kubernetes-tools.svg)](https://github.com/unixorn/kubernetes-tools/stargazers)
+![Contributors](https://img.shields.io/github/contributors/unixorn/kubernetes-tools.svg)
+[![GitHub last commit](https://img.shields.io/github/last-commit/unixorn/kubernetes-tools/main.svg)](https://github.com/unixorn/kubernetes-tools)
+
 Miscellaneous scripts for working with kubernetes
+
+## Scripts
+
+| Name                  | Description                                                  |
+| --------------------- | ------------------------------------------------------------ |
+| `k8s-get-certificate` | Extract a cluster's internal CA that issues API certificates |
---
+++ kubernetes-tools.plugin.zsh
@@ -0,0 +1,26 @@
+# Copyright 2025 Joseph Block <jpb@apesseekingknowledge.net>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Add our plugin's bin diretory to user's path per
+# https://zdharma-continuum.github.io/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html#zero-handling
+
+0="${ZERO:-${${0:#$ZSH_ARGZERO}:-${(%):-%N}}}"
+0="${${(M)0:#/*}:-$PWD/$0}"
+
+local k8s_tools_bin="${0:h}/bin"
+
+if [[ -z "${path[(r)${k8s_tools_bin}]}" ]]; then
+    path+=( "${k8s_tools_bin}" )
+fi
+
